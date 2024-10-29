1e23aed1ecc77bace3b619371f0f1c2786ddc53d
MarioCriado <mariocriadoguerrero26@gmail.com>
2024-10-29 16:42:14 +0100

    Cambios realizados

--- Funciones.py
+++ Funciones.py
@@ -4,4 +4,23 @@ Autor: Mario Criado Guerrero
 Clase: Diseño de interfaces
 """
 
+import requests
 
+class PersonajesAPI:
+    def __init__(self, url):
+        self.url = url
+
+    def listar_personajes(self):
+        try:
+            respuesta = requests.get(self.url)
+            respuesta.raise_for_status()
+            personajes = respuesta.json()
+            for personaje in personajes:
+                print(f"Nombre: {personaje['nombre']}, Nacimiento: {personaje['Nacimiento']}, Jinchūriki: {personaje['Jinchūriki']}, Clan: {personaje['Clan']}, Aldea: {personaje['Aldea']}")
+        except requests.exceptions.RequestException as e:
+            print(f"Ocurrió un error al obtener los personajes: {e}")
+
+if __name__ == "__main__":
+    api_url = "http://127.0.0.1:5000/api/personajes"
+    api = PersonajesAPI(api_url)
+    api.listar_personajes()
